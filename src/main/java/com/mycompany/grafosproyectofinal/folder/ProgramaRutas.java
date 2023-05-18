4b03c6a7492c35706fa7564ec36f3fa750d62df0
NaelyRubio <guimelrubio021@gmail.com>
2023-05-18 16:11:55 -0700

    Documentacion de la clase ProgramaRutas
    ay do

--- src/main/java/com/mycompany/grafosproyectofinal/folder/ProgramaRutas.java
+++ src/main/java/com/mycompany/grafosproyectofinal/folder/ProgramaRutas.java
@@ -14,25 +14,44 @@ import javax.lang.model.SourceVersion;
 
 public class ProgramaRutas {
 
+    //Clase interna ciudad. 
     class Ciudad {
 
+        
+    //atributos de la clase interna
     }
     String nombre;
     List<Colindancia> colindancias;
 
+    
+    /**
+     * Constructor que recibe paramos de nombre 
+     * @param nombre asigna el valor al parametro nombre
+     */
     public ProgramaRutas (String nombre) {
         this.nombre = nombre;
+        //crea una nueva instancia de arraylist y le asigna el atributo colindancia
         this.colindancias = new ArrayList<>();
     }
 
     
 }
 
+/**
+ * Clase colindancia
+ * @author Guimel Naely Rubio Morillon - 229324
+ */
 class Colindancia {
+    
+    //Areibutos de la clase 
     Ciudad ciudad;
     int distancia;
     int cuotaPeaje;
 
+    /*
+    * Constructor que recibe parametros de tipo ciudad, dostancia, cuotapeaje
+    *
+    */
     public Colindancia(Ciudad ciudad, int distancia, int cuotaPeaje) {
         this.ciudad = ciudad;
         this.distancia = distancia;
